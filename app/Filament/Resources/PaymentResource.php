3cc36bdc95317c08c94855a8484cc35b61b1d3b5
ayyub <ayoubhumeid@gmail.com>
2026-03-08 16:31:43 +0200

    change column name that get the remaing amount of lease

--- app/Filament/Resources/PaymentResource.php
+++ app/Filament/Resources/PaymentResource.php
@@ -284,7 +284,7 @@ class PaymentResource extends Resource
                     ->color('success'),
 
                 Tables\Columns\TextColumn::make('remaining_amount')
-                    ->label('Remaining after this payment')
+                    ->label('Remaining')
                     ->money('USD')
                     ->sortable()
                     ->color(fn($state) => $state > 0 ? 'danger' : 'success'),
